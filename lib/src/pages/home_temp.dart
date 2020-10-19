218a9bb1ef9765e8ae270def71660d38600caf08
migporuiz@gmail.com <58771720+MigPorteroDevelop@users.noreply.github.com>
2020-10-19 13:26:49 +0200

    json

--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -1,27 +1,51 @@
 import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget {
+  final opciones = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Componentes'),
       ),
-      body: ListView(
+      body: ListView(children: _crearItemsCorta()),
+    );
+  }
+
+  /* List<Widget> _crearItems() {
+    List<Widget> lista = new List<Widget>();
+
+    for (String opt in opciones) {
+      final tempWidget = ListTile(
+        title: Text(opt),
+      );
+      lista
+        ..add(tempWidget)
+        ..add(Divider(
+          height: 1.0,
+          color: Colors.indigoAccent,
+          indent: 13.0,
+          endIndent: 300.0,
+        ));
+    }
+    return lista;
+  }*/
+
+  List<Widget> _crearItemsCorta() {
+    return opciones.map((item) {
+      return Column(
         children: <Widget>[
           ListTile(
-            title: Text('Listtile title'),
+            title: Text(item + '!'),
+            subtitle: Text('Cualquier cosa'),
+            leading: Icon(Icons.account_box),
+            trailing: Icon(Icons.keyboard_arrow_right),
+            onTap: () {},
           ),
-          Divider(),
-          ListTile(
-            title: Text('Listtile title'),
-          ),
-          Divider(),
-          ListTile(
-            title: Text('Listtile title'),
-          ),
+          Divider()
         ],
-      ),
-    );
+      );
+    }).toList();
   }
 }
